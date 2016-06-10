cb8fd2d6e8e3c357c33ac3b36b066f5e07d91a0c
Logan <logan@thinkingbox.ca>
2016-06-10 14:15:37 -0700

    Vis

--- src/components/vis/vis.js
+++ src/components/vis/vis.js
@@ -8,6 +8,7 @@ export default class Vis extends Component {
 	constructor() {
 		super(); 
 		this.treeful = new Treeful();
+		this.treeful.destroy();
 
 		this.currentXPos = window.innerWidth / 2;
 		
@@ -26,6 +27,7 @@ export default class Vis extends Component {
 		this.initialX = this.parentX;
 
 		this.parentGenerated = false;
+		this.offsetX = 0;
 
 		this.keys = [];
 
@@ -33,36 +35,29 @@ export default class Vis extends Component {
 	}
 
 	componentDidMount() {
-
+		this.treeful = new Treeful();
 		this.c = this.refs.canvas;
 		this.context = this.c.getContext('2d');
 
-	}
-
-	createParentNode() {
-		this.parentGenerated = true;
-		let node = new TreefulNode('parent', 20);
-		this.treeful.addParentNode(node);
-		// this.visualize();
 		this.drawFirstNode();
 		let opt = document.createElement('option');
-		opt.innerHTML = 'parent';
+		opt.innerHTML = 'root';
 		this.refs.option.appendChild(opt);
 
 	}
 
 	createChildNode() {
+
 		let id = this.refs.childName.value;
 		let parent = this.refs.option.value;
 
-		if(id == "" || parent == "" || this.keys.indexOf(id) > -1 || !this.parentGenerated) return;
+		if(id == "" || parent == "" || this.keys.indexOf(id) > -1) return;
 
 		this.keys.push(id);
 
 		this.refs.childName.value += "_1";
 
-		let node = new TreefulNode(id, 20);
-		this.treeful.addChildNode(parent, node);
+		this.treeful.addNode(id, 20, parent);
 		this.visualize();
 
 		let opt = document.createElement('option');
@@ -79,10 +74,11 @@ export default class Vis extends Component {
 		this.parentY = this.currentYPos;
 
 		let tree = this.treeful.getTree();
+		console.log(tree);
 		
 		this.context.clearRect(0,0,this.c.width, this.c.height);
 		this.drawFirstNode();
-		this.recursiveDraw(tree['parent']);
+		this.recursiveDraw(tree);
 
 	}
 
@@ -94,9 +90,15 @@ export default class Vis extends Component {
 		//Go down once for direct children
 		this.currentYPos += this.childRadius * 2 + this.distanceBetweenNodes;
 
+		this.offsetX = 0;
+		
 		if(c > 0) {
 
+			// this.offsetX = (c - (c%2==1?1:0)) * (this.childRadius * 2 + this.distanceBetweenNodes) / 2;
+
 			for(let key in node['children']) {
+				this.currentXPos;
+
 				this.drawNode(key);
 
 				let c2 = Object.keys(node['children'][key]['children']).length;
@@ -104,38 +106,42 @@ export default class Vis extends Component {
 				if(c2 > 0) {
 					this.parentY = this.currentYPos;
 					this.parentX = this.currentXPos;
-					this.recursiveDraw(node['children'][key]);
+					this.recursiveDraw(node['children'][key]);	
 				} 
-				this.currentXPos += this.childRadius * 2 + this.distanceBetweenNodes;	
-			}
 
+
+				this.currentXPos += (this.childRadius * 2 + this.distanceBetweenNodes) ;	
+
+			}
+			
 			this.currentYPos -= this.childRadius * 2 + this.distanceBetweenNodes;
 			this.currentXPos -= c * (this.childRadius * 2 + this.distanceBetweenNodes);
-			
 
 
-		} 
+		} 		
 
 		this.parentY = this.initialY;
 
 		this.parentX = this.initialX;
+
+
 
 	}
 
 	drawNode(text) {
 		this.context.beginPath();
 		this.context.fillStyle = '#677077';
-		this.context.arc(this.currentXPos, this.currentYPos, this.childRadius, 0, 2*Math.PI);
+		this.context.arc(this.currentXPos - this.offsetX, this.currentYPos, this.childRadius, 0, 2*Math.PI);
 		this.context.fill();
 
 		this.context.font = '15px arial';
 		this.context.fillStyle = 'black';
-		this.context.fillText(text, (this.currentXPos - this.childRadius / 2), this.currentYPos);
+		this.context.fillText(text, ((this.currentXPos - this.offsetX ) - this.childRadius / 2), this.currentYPos);
 
 
 		this.context.beginPath();
 		this.context.moveTo(this.parentX, this.parentY);
-		this.context.lineTo(this.currentXPos, this.currentYPos);
+		this.context.lineTo((this.currentXPos - this.offsetX), this.currentYPos);
 		this.context.stroke();
 	}
 
@@ -151,10 +157,7 @@ export default class Vis extends Component {
 		return (
 			<div className='content vis'>
 				<div className='controls'>
-					<div className='section'>
-						<button onClick={this.createParentNode.bind(this)}>Add Parent Node</button>
-					</div>
-					
+
 					<div className='section'>
 						<span>Add a child named: </span>
 						<input type='text' ref='childName' placeholder='name'/>
